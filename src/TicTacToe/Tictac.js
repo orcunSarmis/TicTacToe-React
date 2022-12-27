cc8e55a47101d661c67afb6eb32076e9991c06b8
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-12-27 20:14:50 +1000

    adding winner details

--- src/TicTacToe/Tictac.js
+++ src/TicTacToe/Tictac.js
@@ -1,39 +1,76 @@
-import React from 'react'
-import './Tictac.css'
+import React, { useState } from "react";
+import "./Tictac.css";
 
 const Tictac = () => {
+  const [turn, setTurn] = useState("x");
+  const [cells, setCells] = useState(Array(9).fill(''));
 
-  const handleClick = () => {
-  
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagnol: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ]
+    }
   }
-  
-  const Cell = () => {
-    return <td>-</td>
-  }
+
+  const handleClick = (num) => {
+    if (cells[num] !== '') {
+      alert('already clicked');
+      return;
+    }
+    let squares = [...cells];
+
+    if (turn === "x") {
+      squares[num] = 'x';
+      setTurn("o");
+    } else {
+      squares[num] = 'o';
+      setTurn('x');
+    }
+
+    checkForWinner(squares); 
+    setCells(squares);
+  };
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <table>
+        Turn: {turn}
         <tbody>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
           </tr>
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default Tictac;
